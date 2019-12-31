8f4bade2463475b2a56e70099f34fc4bf763fb0f
Georg Schramm <georg.schramm@posteo.net>
2019-12-31 14:11:02 +0100

    get nearest neighbors

--- bowsher.py
+++ bowsher.py
@@ -1,22 +1,25 @@
 # utils to implement the symmetric and asymmetric Bowsher prior
 
 import numpy as np
+np.random.seed(0)
 
-img = np.arange(8*7).reshape(8,7)
-#s   = np.array([[0,1,0],[1,0,1],[0,1,0]])
-s = np.array([[0,0,1,0,0],[0,1,1,1,0],[1,1,0,1,1],[0,1,1,1,0],[0,0,1,0,0]])
+#img      = np.arange(8*7).reshape(8,7)
+img      = np.random.rand(5,5)
+#s       = np.array([[0,1,0],[1,0,1],[0,1,0]])
+s        = np.array([[0,0,1,0,0],[0,1,1,1,0],[1,1,0,1,1],[0,1,1,1,0],[0,0,1,0,0]])
+nnearest = 3
 
 ndim  = s.ndim
 sinds = np.where(s == 1)
 
 nneighbors = sinds[0].shape[0] 
 
-vox    = [7,6]
+vox    = (4,4)
 offset = (np.array(s.shape) // 2)
 
 inds = []
 for j in range(nneighbors):
-  tmp       = np.zeros(ndim, dtype = np.int64)
+  tmp       = np.zeros(ndim, dtype = np.uint16)
   is_inside = True
 
   for i in range(ndim):
@@ -30,8 +33,21 @@ for j in range(nneighbors):
   if is_inside:
     inds.append(tmp)  
 
+# inds now contains the indicies of the neighbors defined by the mask
+# around our given voxel
 inds = tuple(np.array(inds).transpose().tolist())
+
+# calculate absolute intensity difference to central voxel
+absdiff = np.abs(img[inds] - img[vox])
+
+# contruct the nearest neighbor indices in the mask neighborhood
+ninds = np.array(inds)[:,np.argsort(absdiff)[:nnearest]]
+ninds = tuple(ninds.tolist())
+
+# convert nearest neighbor inds to 1d flattened index
+ninds_1d = np.ravel_multi_index(ninds,img.shape)
 
 print(img)
 print(img[vox[0],vox[1]])
-print(img[inds])
+#print(img[inds])
+print(img[np.unravel_index(ninds_1d, img.shape)])
