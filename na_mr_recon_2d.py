2e0625fec3c56008ec049b83b6c502f0507e8ff9
Georg Schramm <georg.schramm@kuleuven.be>
2020-01-07 17:44:18 +0100

    continur apodized MR recon with bowsher prior example

--- na_mr_recon_2d.py
+++ na_mr_recon_2d.py
@@ -6,8 +6,12 @@ from scipy.optimize import fmin_l_bfgs_b
 import matplotlib.pyplot as py
 from   matplotlib.colors import LogNorm
 
-from apodized_fft_2d import *
+from apodized_fft_2d   import *
+from nearest_neighbors import *
+from bowsher           import *
+
 from pymirc.image_operations import zoom3d, complex_grad, complex_div
+
 
 py.ion()
 py.rc('image', cmap='gray')
@@ -63,7 +67,52 @@ def mr_data_fidelity_grad(recon, readout_inds, apo_imgs):
   grad     = adjoint_apodized_fft_2d(diff, readout_inds, apo_imgs_recon)
 
   if isflat:
-    grad  = complex2d_to_flat_real(grad)
+    grad   = complex2d_to_flat_real(grad)
+    recon  = complex2d_to_flat_real(recon)
+
+  return grad
+
+#--------------------------------------------------------------------
+def mr_bowsher_cost(recon, readout_inds, apo_imgs, beta, ninds, ninds2, method):
+  # ninds2 is a dummy argument to have the same arguments for
+  # cost and its gradient
+
+  cost = mr_data_fidelity(recon, readout_inds, apo_imgs)
+
+  if beta > 0:
+    isflat = False
+    if recon.ndim == 1:  
+      isflat = True
+      recon  = flat_real_to_complex2d(recon)
+
+    cost += beta*bowsher_prior_cost(recon.real, ninds, method)
+    cost += beta*bowsher_prior_cost(recon.imag, ninds, method)
+
+    if isflat:
+      recon  = complex2d_to_flat_real(recon)
+   
+  return cost
+
+#--------------------------------------------------------------------
+def mr_bowsher_grad(recon, readout_inds, apo_imgs, beta, ninds, ninds2, method):
+
+  grad = mr_data_fidelity_grad(recon, readout_inds, apo_imgs)
+
+  if beta > 0:
+    isflat = False
+    if recon.ndim == 1:  
+      isflat = True
+      recon  = flat_real_to_complex2d(recon)
+
+    prior_grad      = np.zeros(recon.shape, dtype = np.complex128)
+    prior_grad.real = bowsher_prior_grad(recon.real, ninds, ninds2, method)
+    prior_grad.imag = bowsher_prior_grad(recon.imag, ninds, ninds2, method)
+
+    if isflat:
+      prior_grad  = complex2d_to_flat_real(prior_grad)
+      recon = complex2d_to_flat_real(recon)
+
+    grad += beta*prior_grad
 
   return grad
 
@@ -130,30 +179,54 @@ signal = signal + noise_level*(np.random.randn(256,256) + np.random.randn(256,25
 #--- do the recon
 
 alg   = 'lbfgs'
-niter = 2000
-lam   = 1e-2
+niter = 500
 
 T2star_recon = T2star.copy()
 #T2star_recon = np.zeros(T2star.shape) + T2star.max()
+#T2star_recon = np.zeros(T2star.shape) + 12
 
 apo_imgs_recon = apo_images(readout_times, T2star_recon)
 
-recon  = np.fft.ifft2(signal) * np.sqrt(np.prod(f.shape)) / np.sqrt(4*signal.ndim)
+init_recon  = np.fft.ifft2(signal) * np.sqrt(np.prod(f.shape)) / np.sqrt(4*signal.ndim)
+
+# --- set up stuff for the prior
+aimg  = (f.max() - f.copy())
+aimg += 0.001*aimg.max()*np.random.random(aimg.shape)
+
+# beta = 1e-4 reasonable for inverse crime
+beta = 1e-4
+s    = np.array([[1,1,1], 
+                 [1,0,1], 
+                 [1,1,1]])
+nnearest = 3 
+method   = 0
+
+ninds  = np.zeros((np.prod(aimg.shape),nnearest), dtype = np.uint32)
+nearest_neighbors(aimg,s,nnearest,ninds)
+ninds2 = is_nearest_neighbor_of(ninds)
+
 
 if alg == 'lbfgs':
   print('starting bfgs')
-  recon = complex2d_to_flat_real(recon)
+  recon = complex2d_to_flat_real(init_recon)
+  recon = np.ones(recon.shape)
 
-  res = fmin_l_bfgs_b(mr_data_fidelity, recon, fprime = mr_data_fidelity_grad, 
-                      args = (readout_inds, apo_imgs_recon), maxfun = niter, maxiter = niter, disp = 1)
+  res = fmin_l_bfgs_b(mr_bowsher_cost,
+                      recon, 
+                      fprime = mr_bowsher_grad, 
+                      args = (readout_inds, apo_imgs_recon, beta, ninds, ninds2, method), 
+                      maxfun = niter, maxiter = niter, disp = 1)
 
   recon = flat_real_to_complex2d(res[0])
-elif alg == 'landweber':
+elif alg == 'gradient_descent':
   step  = 0.2
+  recon = init_recon.copy()
 
   for it in range(niter):
-    recon = recon - step*mr_data_fidelity_grad(recon, readout_inds, apo_imgs_recon)
-    cost  = mr_data_fidelity(recon, readout_inds, apo_imgs_recon)
+    #recon = recon - step*mr_data_fidelity_grad(recon, readout_inds, apo_imgs_recon)
+    recon = recon - step*mr_bowsher_grad(recon, readout_inds, apo_imgs_recon, beta, ninds, ninds2, method)
+    #cost  = mr_data_fidelity(recon, readout_inds, apo_imgs_recon)
+    cost  = mr_bowsher_cost(recon, readout_inds, apo_imgs_recon, beta, ninds, ninds2, method)
     print(it + 1, niter, round(cost,5))
 
 
@@ -215,48 +288,38 @@ elif alg == 'landweber':
 #
 #    recons[it + 1, ...] = recon
 #
-##----------------------------------------------------------
-##--- plot the results
-#
-#vmax = min(1.5*f.max(),max(f.max(),np.abs(recon).max()))
-#
-#fig, ax = py.subplots(3,3,figsize = (9,9))
-#ax[0,0].imshow(f, vmin = 0, vmax = vmax)
-#ax[0,1].imshow(np.abs(recons[0,:]), vmin = 0, vmax = vmax)
-#ax[0,2].imshow(np.abs(recon), vmin = 0, vmax = vmax)
-#ax[1,0].imshow(np.abs(recon) - f, vmin = -0.1*vmax, vmax = 0.1*vmax, cmap = py.cm.bwr)
-#ax[1,1].plot(f[:,128],'k')
-#ax[1,1].plot(np.abs(recons[0,...])[:,128],'b:')
-#ax[1,1].plot(np.abs(recon)[:,128],'r:')
-#ax[1,2].plot(f[128,:],'k')
-#ax[1,2].plot(np.abs(recons[0,...])[128,:],'b:')
-#ax[1,2].plot(np.abs(recon)[128,:],'r:')
-#ax[2,0].imshow(T2star, vmin = 0, vmax = T2star.max())
-#ax[2,1].imshow(T2star_recon, vmin = 0, vmax = T2star.max())
+
+
+
+#----------------------------------------------------------
+#--- plot the results
+
+vmax = min(1.5*f.max(),max(f.max(),np.abs(recon).max()))
+
+fig, ax = py.subplots(3,3,figsize = (9,9))
+ax[0,0].imshow(f, vmin = 0, vmax = vmax)
+ax[0,1].imshow(np.abs(init_recon), vmin = 0, vmax = vmax)
+ax[0,2].imshow(np.abs(recon), vmin = 0, vmax = vmax)
+ax[1,0].imshow(np.abs(recon) - f, vmin = -0.1*vmax, vmax = 0.1*vmax, cmap = py.cm.bwr)
+ax[1,1].plot(f[:,128],'k')
+ax[1,1].plot(np.abs(init_recon)[:,128],'b:')
+ax[1,1].plot(np.abs(recon)[:,128],'r:')
+ax[1,2].plot(f[128,:],'k')
+ax[1,2].plot(np.abs(init_recon)[128,:],'b:')
+ax[1,2].plot(np.abs(recon)[128,:],'r:')
+ax[2,0].imshow(T2star, vmin = 0, vmax = T2star.max())
+ax[2,1].imshow(T2star_recon, vmin = 0, vmax = T2star.max())
 #ax[2,2].plot(cost[10:])
-#
-#ax[0,0].set_title('ground truth')
-#ax[0,1].set_title('init recon')
-#ax[0,2].set_title('iterative recon')
-#ax[2,0].set_title('ground truth T2*')
-#ax[2,1].set_title('recon T2*')
-#ax[2,2].set_title('cost')
-#
-#fig.tight_layout()
-#fig.show()
-#
-#
-#
-###----------------------------------------------------------
-###--- adjoint test
-##n = 256
-##f = np.random.rand(n,n) + np.random.rand(n,n)*1j
-##F = np.random.rand(n,n) + np.random.rand(n,n)*1j
-##
-##f_fwd  = apodized_fft_2d(f, readout_inds, apo_imgs)
-##F_back = adjoint_apodized_fft_2d(F, readout_inds, apo_imgs)
-##
-##print((f_fwd * F.conj()).sum())
-##print((f * F_back.conj()).sum())
+
+ax[0,0].set_title('ground truth')
+ax[0,1].set_title('init recon')
+ax[0,2].set_title('iterative recon')
+ax[2,0].set_title('ground truth T2*')
+ax[2,1].set_title('recon T2*')
+ax[2,2].set_title('cost')
+
+fig.tight_layout()
+fig.show()
+
 
 
