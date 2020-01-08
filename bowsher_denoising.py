01faed265d7d02d296b42e5fba2a39812585a28a
Georg Schramm <georg.schramm@kuleuven.be>
2020-01-08 13:48:35 +0100

    improve bowsher denoising example with better data

--- bowsher_denoising.py
+++ bowsher_denoising.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from scipy.optimize    import fmin_bfgs
+from scipy.optimize    import fmin_l_bfgs_b
 from nearest_neighbors import *
 from bowsher           import *
 
@@ -31,9 +31,19 @@ def bowsher_denoising_grad(img, noisy_img, beta, ninds, ninds2, method):
 #--------------------------------------------------------------------
 
 np.random.seed(0)
-nnearest = 2 
-aimg     = np.pad(np.ones((20,20)),7,'constant')
-aimg    += 0.01*np.random.random(aimg.shape)
+
+# load the brain web labels
+data  = np.load('54.npz')
+gt    = data['arr_0'][...,65:75]
+#gt    = data['arr_0'][...,75]
+aimg  = (gt.max() - gt)**0.5
+aimg += 0.001*aimg.max()*np.random.randn(*gt.shape)
+img   = gt + 0.1*gt.max()*np.random.randn(*gt.shape)
+
+# parameter for the bowsher prior
+beta     = 1e0
+nnearest = 3 
+method   = 0
 
 if aimg.ndim == 3:
   s = np.array([[[0,1,0],[1,1,1],[0,1,0]], 
@@ -41,60 +51,50 @@ if aimg.ndim == 3:
                 [[0,1,0],[1,1,1],[0,1,0]]])
 
 else:
-  s = np.array([[0,1,0], 
+  s = np.array([[1,1,1], 
                 [1,0,1], 
-                [0,1,0]])
+                [1,1,1]])
 
 ninds  = np.zeros((np.prod(aimg.shape),nnearest), dtype = np.uint32)
 nearest_neighbors(aimg,s,nnearest,ninds)
 ninds2 = is_nearest_neighbor_of(ninds)
 
-#---
+cost = []
+cb   = lambda x: cost.append(bowsher_denoising_cost(x, img.flatten(), beta, ninds, ninds2, method))
 
-img        = 2*(1 - aimg) + 1
-noisy_img  = img + 0.5*np.random.random(aimg.shape)
-init_img   = noisy_img.copy()
+bounds = [(0,None)] * len(img.flatten())
 
-##---
-##---
-#x0 = noisy_img.copy()
-#x1 = x0.copy()
-#tmp      = (16,16)
-#eps      = 1e-5
-#x1[tmp] += eps
-#
-#method = 1
-#
-#p0 = bowsher_prior_cost(x0, ninds, method)
-#p1 = bowsher_prior_cost(x1, ninds, method)
-#
-#g  = bowsher_prior_grad(x0, ninds, ninds2, method)
-#
-##---
-##---
-
-method = 0
-beta   = 1e0
-
-res = fmin_bfgs(bowsher_denoising_cost, init_img.flatten(), fprime = bowsher_denoising_grad, 
-                args = (noisy_img.flatten(), beta, ninds, ninds2, method), maxiter = 100,
-                retall = True)
+res = fmin_l_bfgs_b(bowsher_denoising_cost, 
+                    img.flatten(), 
+                    fprime = bowsher_denoising_grad, 
+                    args = (img.flatten(), beta, ninds, ninds2, method),
+                    bounds = bounds,
+                    callback = cb,
+                    disp = 1)
 
 denoised_img = res[0].reshape(aimg.shape)
 
-cost = [bowsher_denoising_cost(x, noisy_img.flatten(), beta, ninds, ninds2, method) for x in res[1]]
+#--- show results
 
 import matplotlib.pyplot as py
+py.rcParams['image.cmap'] = 'Greys_r'
 fig, ax = py.subplots(2,2,figsize=(7,7))
-im_kwargs = {'vmin':0, 'vmax':noisy_img.max(), 'cmap':py.cm.Greys}
-ax[0,0].imshow(img, **im_kwargs )
-ax[0,1].imshow(aimg, **im_kwargs)
-ax[1,0].imshow(noisy_img, **im_kwargs)
-ax[1,1].imshow(denoised_img, **im_kwargs)
+im_kwargs = {'vmin':0, 'vmax':1.3*gt.max(), 'cmap':py.cm.Greys_r}
+if gt.ndim == 2:
+  ax[0,0].imshow(gt.transpose(), **im_kwargs )
+  ax[0,1].imshow(aimg.transpose())
+  ax[1,0].imshow(img.transpose(), **im_kwargs)
+  ax[1,1].imshow(denoised_img.transpose(), **im_kwargs)
+else: 
+  sl = gt.shape[-1]//2
+  ax[0,0].imshow(gt[...,sl].transpose(), **im_kwargs )
+  ax[0,1].imshow(aimg[...,sl].transpose())
+  ax[1,0].imshow(img[...,sl].transpose(), **im_kwargs)
+  ax[1,1].imshow(denoised_img[...,sl].transpose(), **im_kwargs)
 fig.tight_layout()
 fig.show()
 
 fig2, ax2 = py.subplots(1,1)
-ax2.loglog(cost)
+ax2.loglog(np.arange(1,len(cost)+1),cost)
 fig2.tight_layout()
 fig2.show()
