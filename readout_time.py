b8117d0f826c34524ea6247a3cb519428d498ee8
Georg Schramm <georg.schramm@posteo.net>
2020-04-06 09:13:24 +0200

    correct and simplify readouttime script

--- readout_time.py
+++ readout_time.py
@@ -1,52 +1,49 @@
 import numpy as np
 
-def Gamma(k, eta, c1, c2):
-  return c1*np.exp(-eta*(k**3)) + c2
+def readout_time(k, 
+                 eta          = 0.9830, 
+                 c1           = 0.54, 
+                 c2           = 0.46, 
+                 alpha_sw_tpi = 18.95,
+                 beta_sw_tpi  = -0.5171,
+                 t0_sw        = 0.0018):
 
-def readout_time(k, eta = 0.5387, c1 = 0.54, c2 = 0.46, 
-                    p = 0.4, G = 0.16, gamma = 1126, Km = 1.8):
 
-  A = c2*(k**3) - (c1/eta)*np.exp(-eta*(k**3)) + (c1/eta)*np.exp(-eta*((p*Km)**3)) - c2*(p*Km)**3
-  B = 3*Gamma(p*Km, eta, c1, c2)*(p**2)*(Km**3)
-  C = Km/(gamma*G)
-
-  return C*(p + A/B)
+  return t0_sw + ((c2*(k**3) - ((c1/eta)*np.exp(-eta*(k**3))) - beta_sw_tpi) / (3*alpha_sw_tpi))
 
 if __name__ == '__main__':
   import matplotlib.pyplot as py
 
   nk  = 64
-  T2s = 0.030
-  T2f = 0.008 
+  T2s = 0.029
+  T2f = 0.009 
+  osp = 1.8
+  Km  = osp*0.8197
 
-  rkwargs = {'eta'   : 0.5387, 
-             'c1'    : 0.54, 
-             'c2'    : 0.46, 
-             'p'     : 0.4, 
-             'G'     : 0.16, 
-             'gamma' : 1126., 
-             'Km'    : 1.8}
-
-
-  k = np.linspace(0, rkwargs['Km'], nk)
+  rkwargs = {'eta'          : 0.9830, 
+             'c1'           : 0.54, 
+             'c2'           : 0.46, 
+             'alpha_sw_tpi' : 18.95,
+             'beta_sw_tpi'  : -0.5171,
+             't0_sw'        : 0.0018}
+              
+  k = np.linspace(0, Km, nk)
   t = readout_time(k, **rkwargs)
 
   decay_env = 0.6*np.exp(-t/T2f) + 0.4*np.exp(-t/T2s)
 
   fig, ax = py.subplots(1,3, figsize = (9,3))
-  ax[0].plot(t,k,'.')
+  ax[0].plot(1000*t,k,'.')
   ax[0].set_xlim((0,None))
-  #ax[0].axvline(44, color = 'k')
 
-  ax[1].plot(k,t,'.')
-  #ax[1].axhline(44, color = 'k')
+  ax[1].plot(k,1000*t,'.')
 
   ax[2].plot(k,decay_env,'.')
 
-  ax[0].set_xlabel('t')
+  ax[0].set_xlabel('t (ms)')
   ax[0].set_ylabel('k')
   ax[1].set_xlabel('k')
-  ax[1].set_ylabel('t')
+  ax[1].set_ylabel('t( ms)')
   ax[2].set_xlabel('k')
   ax[2].set_ylabel('decay env.')
 
