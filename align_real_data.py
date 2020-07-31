10a8c6894da280e8e6c861766fec01d1c8caa833
Georg Schramm <georg.schramm@kuleuven.be>
2020-07-31 17:46:15 +0200

    set default grid size to 256 when aligning data

--- align_real_data.py
+++ align_real_data.py
@@ -12,6 +12,7 @@ import pymirc.viewer as pv
 
 pdir = os.path.join('data','sodium_data','BT-007_visit2')
 sdir = 'PhyCha_kw1'
+n    = 256
 
 sdir1 = os.path.join(glob(os.path.join(pdir,'*TE03'))[0], sdir.split('_')[0], sdir)
 sdir2 = os.path.join(glob(os.path.join(pdir,'*TE5'))[0], sdir.split('_')[0], sdir)
@@ -50,15 +51,15 @@ for i, fname2 in enumerate(fnames2):
 na2 = np.flip(np.abs(data2).sum(axis = 0), (0,1))
 na2 /= na_norm
 
-
 #----------------------------------------------------------------------
 # align the T1 to the sodium image
 
-# interpolate sodium image to a 256 grid
-na_interp  = zoom(na,  2, order = 1, prefilter = False)
-na2_interp = zoom(na2, 2, order = 1, prefilter = False)
-
 fov = 223.
+
+# interpolate sodium image to a 256 grid
+na_interp  = zoom(na,  n//64, order = 1, prefilter = False)
+na2_interp = zoom(na2, n//64, order = 1, prefilter = False)
+
 
 na_voxsize = fov / np.array(na_interp.shape)
 
@@ -85,8 +86,8 @@ t1_na_grid  = pi.aff_transform(t1, regis_aff, na_interp.shape, cval = t1.min())
 # save the data
 
 #nib.save(nib.Nifti1Image(csf_na_grid, na_affine), './data/SodiumExample/csf_128_aligned.nii')
-nib.save(nib.Nifti1Image(t1_na_grid, na_affine), os.path.join(pdir,'mprage_128_aligned.nii'))
-nib.save(nib.Nifti1Image(na_interp, na_affine), os.path.join(pdir,'TE03_128_' + sdir + '.nii'))
-nib.save(nib.Nifti1Image(na2_interp, na_affine), os.path.join(pdir,'TE5_128_' + sdir + '.nii'))
+nib.save(nib.Nifti1Image(t1_na_grid, na_affine), os.path.join(pdir,f'mprage_{n}_aligned.nii'))
+nib.save(nib.Nifti1Image(na_interp, na_affine), os.path.join(pdir,f'TE03_{n}_' + sdir + '.nii'))
+nib.save(nib.Nifti1Image(na2_interp, na_affine), os.path.join(pdir,f'TE5_{n}_' + sdir + '.nii'))
  
-np.savetxt(os.path.join(pdir,'affine_128.txt'), regis_aff)
+np.savetxt(os.path.join(pdir,'affine_{n}.txt'), regis_aff)
