3197b308af0e9f4b56325a9c8c733b0438bf6212
Georg Schramm <georg.schramm@kuleuven.be>
2021-04-22 19:05:04 +0000

    add lookup tables for readout times

--- readout_time.py
+++ readout_time.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def readout_time(k, 
+                 lookup       = None,
                  eta          = 0.9830, 
                  c1           = 0.54, 
                  c2           = 0.46, 
@@ -8,18 +9,25 @@ def readout_time(k,
                  beta_sw_tpi  = -0.5171,
                  t0_sw        = 0.0018):
 
+  if lookup is not None:
+    tmp = np.loadtxt(lookup)
+    x = tmp[:,1]
+    y = tmp[:,0] / 1000
 
-  # the point until the readout is linear
-  m = 1126 * 0.16
+    t = np.interp(k,x,y)
 
-  k_lin = t0_sw * m
+  else:
+    # the point until the readout is linear
+    m = 1126 * 0.16
 
-  i1 = np.where(k <= k_lin)
-  i2 = np.where(k > k_lin)
+    k_lin = t0_sw * m
 
-  t = np.zeros(k.shape)
-  t[i1] = k[i1] / m
-  t[i2] = t0_sw + ((c2*(k[i2]**3) - ((c1/eta)*np.exp(-eta*(k[i2]**3))) - beta_sw_tpi) / (3*alpha_sw_tpi))
+    i1 = np.where(k <= k_lin)
+    i2 = np.where(k > k_lin)
+
+    t = np.zeros(k.shape)
+    t[i1] = k[i1] / m
+    t[i2] = t0_sw + ((c2*(k[i2]**3) - ((c1/eta)*np.exp(-eta*(k[i2]**3))) - beta_sw_tpi) / (3*alpha_sw_tpi))
 
   return t
 
@@ -32,12 +40,14 @@ if __name__ == '__main__':
   osp = 1.8
   Km  = osp*0.8197
 
-  rkwargs = {'eta'          : 0.9830, 
-             'c1'           : 0.54, 
-             'c2'           : 0.46, 
-             'alpha_sw_tpi' : 18.95,
-             'beta_sw_tpi'  : -0.5171,
-             't0_sw'        : 0.0018}
+  rkwargs = {'lookup': 'kspace_times_4mm_g32.ksp'}
+
+  #rkwargs = {'eta'          : 0.9830, 
+  #           'c1'           : 0.54, 
+  #           'c2'           : 0.46, 
+  #           'alpha_sw_tpi' : 18.95,
+  #           'beta_sw_tpi'  : -0.5171,
+  #           't0_sw'        : 0.0018}
               
   k = np.linspace(0, Km, nk)
   t = readout_time(k, **rkwargs)
@@ -49,7 +59,7 @@ if __name__ == '__main__':
   ax[0].set_xlim((0,None))
 
   ax[1].plot(k,1000*t)
-  ax[2].axvline(rkwargs['t0_sw']*(1126*0.16), color = 'k', ls = ':')
+  #ax[2].axvline(rkwargs['t0_sw']*(1126*0.16), color = 'k', ls = ':')
   ax[2].plot(k,1000*t)
   ax[2].set_xlim((0,0.5))
   ax[2].set_ylim((0,3.5))
