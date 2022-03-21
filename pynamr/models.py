a456d10065bf8fe3fe8d0195ea389bf3042c95d0
Georg Schramm <georg.schramm@kuleuven.be>
2022-03-21 18:01:40 +0100

    add calculation of readout time to acquisition model

--- pynamr/models.py
+++ pynamr/models.py
@@ -54,20 +54,62 @@ def upsample(x_ds, ds, axis = 0):
 #----------------------------------------------------------------------------------
 
 class DualTESodiumAcqModel:
-  def __init__(self, ds, image_shape, ncoils, sens):
-    # downsample / pooling factor
-    self._ds = ds
+  def __init__(self, image_shape, ncoils, sens, dt):
+    # shape of the data
+    self._data_shape = (64,64,64)
+    # shape of the image
     self._image_shape = image_shape
+    # downsampling factor
+    self._ds = self._image_shape[0] // self._data_shape[0]
+
+    # number coils
     self._ncoils = ncoils
+    # sensitivity "image" for each coil in downsampled data space
     self._sens = sens
+    # time between two echos
+    self._dt = dt
 
-  @property
-  def ds(self):
-    return self._ds 
+    # parameters related to the read out trajectory
+    self._eta            = 0.9830
+    self._c1             = 0.54
+    self._c2             = 0.46 
+    self._alpha_sw_tpi   = 18.95
+    self._beta_sw_tpi    = -0.5171
+    self._t0_sw          = 0.0018
+    self._k_edge         = 1.8*0.8197
+    self._n_readout_bins = 32
 
-  @ds.setter
-  def ds(self, value):
-    self._ds = value
+    self.setup_readout()
+
+  def setup_readout(self):
+    print('setting up readout')
+
+    # setup the readout time and readout indexes
+    k0,k1,k2 = xp.meshgrid(xp.arange(64) - 32 + 0.5, xp.arange(64) - 32 + 0.5, xp.arange(64) - 32 + 0.5)
+    abs_k = xp.sqrt(k0**2 + k1**2 + k2**2)
+    abs_k = xp.fft.fftshift(abs_k)
+
+    # rescale abs_k such that k = 1.8*0.8197 is at r = 32 (the edge)
+    abs_k *= self._k_edge/31.5
+
+    # calculate the readout times and the k-spaces locations that
+    # are read at a given time
+    t_read_3 = self.readout_time(abs_k, self._eta, self._c1, self._c2, self._alpha_sw_tpi,
+                                 self._beta_sw_tpi, self._t0_sw)
+  
+    
+    k_1d = xp.linspace(0, self._k_edge, self._n_readout_bins + 1)
+    
+    self._readout_inds = []
+    self._tr = xp.zeros(self._n_readout_bins)
+    
+    for i in range(self._n_readout_bins):
+      k_start = k_1d[i]
+      k_end   = k_1d[i+1]
+      rinds   = xp.where(xp.logical_and(abs_k >= k_start, abs_k <= k_end))
+    
+      self._tr[i] = t_read_3[rinds].mean()
+      self._readout_inds.append(rinds)
 
   @property
   def image_shape(self):
@@ -76,6 +118,8 @@ class DualTESodiumAcqModel:
   @image_shape.setter
   def image_shape(self, value):
     self._image_shape = value
+    # update downsampling factor when image shape is changed
+    self._ds = self._image_shape[0] // self._data_shape[0]
 
   @property
   def ncoils(self):
@@ -92,6 +136,14 @@ class DualTESodiumAcqModel:
   @sens.setter
   def sens(self, value):
     self._sens = value
+
+  @property
+  def dt(self):
+    return self._dt
+
+  @dt.setter
+  def dt(self, value):
+    self._dt = value
 
   #------------------------------------------------------------------------------
   def forward(self, f, Gam):
@@ -166,6 +218,28 @@ class DualTESodiumAcqModel:
 
     return f
 
+  #------------------------------------------------------------------------------
+  @staticmethod
+  def readout_time(k, eta, c1, c2, alpha_sw_tpi, beta_sw_tpi, t0_sw):
+  
+    # the point until the readout is linear
+    m = 1126 * 0.16
+  
+    k_lin = t0_sw * m
+  
+    i1 = xp.where(k <= k_lin)
+    i2 = xp.where(k > k_lin)
+  
+    t = xp.zeros(k.shape)
+    t[i1] = k[i1] / m
+    t[i2] = t0_sw + ((c2*(k[i2]**3) - ((c1/eta)*xp.exp(-eta*(k[i2]**3))) - beta_sw_tpi) / (3*alpha_sw_tpi))
+
+    # convert to ms
+    t *= 1000
+
+    return t
+
+
 #---------------------------------------------------------------------------------------------
 
 
@@ -178,6 +252,7 @@ if __name__ == '__main__':
   ds = 2
   n  = 128 
   ncoils = 8
+  dt = 5.
 
   a = xp.pad(xp.random.rand(n-4,n-4,n-4),2).astype(xp.float32)
 
@@ -187,7 +262,7 @@ if __name__ == '__main__':
  
   Gam = xp.random.rand(n,n,n).astype(xp.float32)
   
-  m = DualTESodiumAcqModel(ds, a.shape, ncoils, sens)
+  m = DualTESodiumAcqModel(a.shape, ncoils, sens, dt)
   
   f_fwd  = m.forward(f, Gam)
   F      = xp.random.rand(*f_fwd.shape).astype(xp.float32)
