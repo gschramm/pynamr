67296ebdc291e256af95663ea3bbd0a7bbe8545e
Georg Schramm <georg.schramm@kuleuven.be>
2022-06-02 10:21:17 +0200

    add argparser to n4 script

--- n4.py
+++ n4.py
@@ -1,7 +1,16 @@
 import SimpleITK as sitk
 from pathlib import Path
+from argparse import ArgumentParser
 
-fname = Path('data/sodium_data/BT12V6/mprage.nii')
+parser = ArgumentParser()
+parser.add_argument('case')
+parser.add_argument('--sdir', default = 'mprage_proton')
+parser.add_argument('--fname', default = 'T1.nii')
+args = parser.parse_args()
+
+fname = Path('data/sodium_data') / args.case / args.sdir / args.fname
+
+#-------------------------------------------
 
 inputImage = sitk.ReadImage(str(fname), sitk.sitkFloat32)
 image      = inputImage
@@ -14,7 +23,7 @@ numberFittingLevels = 4
 
 corrected_image = corrector.Execute(image, maskImage)
 
-sitk.WriteImage(corrected_image, str(fname.parent / 'T1_n4.nii'))
+sitk.WriteImage(corrected_image, str(fname.parent / '_n4.'.join(fname.name.split('.'))))
 
 import numpy as np
 a = np.flip(np.swapaxes(sitk.GetArrayFromImage(inputImage),0,2), (0,1))
@@ -22,4 +31,4 @@ b = np.flip(np.swapaxes(sitk.GetArrayFromImage(corrected_image),0,2), (0,1))
 
 import pymirc.viewer as pv
 import matplotlib.pyplot as plt
-pv.ThreeAxisViewer([a,b], imshow_kwargs = {'cmap':plt.cm.Greys_r})
+vi = pv.ThreeAxisViewer([a,b], imshow_kwargs = {'cmap':plt.cm.Greys_r})
