7b2cd10776ab6cc712fe78c2eda9c39f7564ec78
Georg Schramm <georg.schramm@kuleuven.be>
2022-06-10 18:41:42 +0200

    wip: add test for gradient for gam (not working yet)

--- pynamr/losses.py
+++ pynamr/losses.py
@@ -1,40 +1,17 @@
+#TODO: f_min_l_bfgs seems to only work with 64bit float 1D arrays -> problem with complex64 data
+
 import numpy as np
 from scipy.optimize import fmin_l_bfgs_b, fmin_cg
-
-class FwdModel:
-  def __init__(self, A):
-    self.A  = A
-    self.ny, self.nx = self.A.shape
-    self.weights = np.eye(self.ny).tolist()
-
-  def __call__(self, x, gam):
-    cost = 0
-
-    for i in range(self.ny):
-      cost += self.weights[i] * (self.A @ ((gam**i) * x))
- 
-    return cost
-
-  def grad_x(self, x, gam, outer_deriv = 1):
-    grad = np.zeros(self.nx)
-
-    for i in range(self.ny):
-      grad += (gam**i) * (self.A.T @ (self.weights[i] * outer_deriv))
-
-    return grad
-
-  def grad_gam(self, x, gam, outer_deriv = 1):
-    grad = np.zeros(self.nx)
-
-    for i in range(self.ny):
-      grad += i * (gam**(i-1)) * x * (self.A.T @ (self.weights[i] * outer_deriv))
-
-    return grad
-
 
 #-------------------------------------------------------------------
 
 class DataFidelityLoss:
+  """ Data fidelity loss for mono exponential dual echo sodium forward model
+     
+      The model is linear in the first argument (the image x), but non linear
+      in the second argument (Gam)
+  """
+
   def __init__(self, model, y):
     self.model = model
     self.y     = y
@@ -43,84 +20,110 @@ class DataFidelityLoss:
     z = self.diff(x, gam)
     return 0.5*(z**2).sum()
 
-  def outer_deriv(self, x):
-    return x
-
   def eval_gam_first(self, gam, x):
     return self.eval_x_first(x, gam)
 
   def diff(self, x, gam):
-    return (self.model(x, gam) - self.y)
+    # reshaping of x/gam is needed since fmin_l_bfgs_b flattens all arrays
+    return (self.model.forward(x.reshape(self.model._image_shape + (2,)) , 
+                               gam.reshape(self.model._image_shape)) - self.y) * self.model._kmask
 
   def grad_x(self, x, gam):
+    in_shape = x.shape
     z = self.diff(x, gam)
-    return self.model.grad_x(x, gam, self.outer_deriv(z))
+
+    # reshaping of x/gam is needed since fmin_l_bfgs_b flattens all arrays
+    return self.model.adjoint(z, gam.reshape(self.model._image_shape)).reshape(in_shape)
 
   def grad_gam(self, gam, x):
+    in_shape = gam.shape
     z = self.diff(x, gam)
-    return self.model.grad_gam(x, gam, self.outer_deriv(z))
+
+    # reshaping of x/gam is needed since fmin_l_bfgs_b flattens all arrays
+    return self.model.grad_gam(z, gam.reshape(self.model._image_shape), x.reshape(self.model._image_shape + (2,))).reshape(in_shape)
 
 
 
 #-------------------------------------------------------------------
 
 if __name__ == '__main__':
+
+  import cupy as cp
+  from scipy.ndimage import gaussian_filter
+  from models import MonoExpDualTESodiumAcqModel
+
   np.random.seed(1)
- 
-  n_outer = 100
-  n_inner = 50
-  nl      = 0
- 
-  x   = np.array([1.5,0.5,2.5,0.8])
-  gam = np.array([2.3,4.7,1.2,3.2])
- 
-  # setup forward model 
-  A   = np.random.rand(5,4)
-  fwd_model = FwdModel(A) 
 
+  data_shape  = (64,64,64)
+  ds          = 2
+  ncoils      = 3
+  dt          = 5.
+  noise_level = 0
+  n_inner     = 20
+  xp          = cp
+
+  n_ds = data_shape[0] 
+  n    = ds*n_ds
+
+  tmp = gaussian_filter(np.pad(np.ones((n//2,n//2,n//2), dtype = np.float64), n//4),2)
+  x   = np.stack([tmp,tmp], axis = -1)
+
+  sens = 1e-2*(np.ones((ncoils,n_ds,n_ds,n_ds)).astype(np.float64) + 1j*np.ones((ncoils,n_ds,n_ds,n_ds)).astype(np.float64))
+ 
+  gam = 0.9*np.ones((n,n,n)).astype(np.float64)
+  
+  fwd_model = MonoExpDualTESodiumAcqModel(data_shape, ds, ncoils, sens, dt, xp)
+  
   # generate data
-  y =  fwd_model(x, gam)
-  data = y + nl*np.random.normal(size = A.shape[0])
+  y = fwd_model.forward(x, gam)
+  data = y + noise_level*np.abs(y).mean()*np.random.randn(*y.shape).astype(np.float64)
 
+  # setup data fidelity loss
   loss = DataFidelityLoss(fwd_model, data)
 
   # inital values
-  x_0   = np.random.rand(A.shape[1])
-  gam_0 = np.random.rand(A.shape[1])
+  x_0   = 1.1*x
+  gam_0 = 0.7*gam
 
-  # test the gradient
+  # check gradients
   ll = loss.eval_x_first(x_0, gam_0)
-  gx = loss.grad_x(x_0,gam_0)
-  gg = loss.grad_gam(gam_0,x_0)
+  gx = loss.grad_x(x_0, gam_0)
+  gg = loss.grad_gam(gam_0, x_0)
 
   eps = 1e-6
 
-  for i in range(A.shape[1]):
-    delta = np.zeros(A.shape[1])
-    delta[i] = eps  
+  delta_x = np.zeros(x.shape)
+  delta_x[64,64,64,0] = eps
+  print(gx[64,64,64,0], (loss.eval_x_first(x_0 + delta_x, gam_0) - ll) / eps)
 
-    print(gx[i], (loss.eval_x_first(x_0 + delta, gam_0) - ll)  / eps)
-    print(gg[i], (loss.eval_gam_first(gam_0 + delta, x_0) - ll)  / eps)
-    print('')
 
-  #----------------------------------------------------------------------------------------------
+  for i in [40,51,63]:
+    delta_g = np.zeros(gam.shape)
+    delta_g[i,i,i] = eps
+    print(gg[i,i,i], (loss.eval_x_first(x_0, gam_0 + delta_g) - ll) / eps)
 
-  x_r   = x_0.copy()
-  gam_r = gam_0.copy()
 
-  # LBFGS
-  #for i_out in range(n_outer):
-  #  res = fmin_l_bfgs_b(loss.eval_x_first, x_r, fprime = loss.grad_x, args = (gam_r,), 
-  #                      maxiter = n_inner, disp = 1)
-  #  x_r = res[0].copy()
 
-  #  res_gam = fmin_l_bfgs_b(loss.eval_gam_first, gam_r, fprime = loss.grad_gam, args = (x_r,), 
+
+  ##----------------------------------------------------------------------------------------------
+
+  #x_r   = x_0.copy()
+  #gam_r = gam_0.copy()
+
+  ## LBFGS
+  ##for i_out in range(n_outer):
+  ##  res = fmin_l_bfgs_b(loss.eval_x_first, x_r, fprime = loss.grad_x, args = (gam_r,), 
+  ##                      maxiter = n_inner, disp = 1)
+  ##  x_r = res[0].copy()
+
+  ##  res_gam = fmin_l_bfgs_b(loss.eval_gam_first, gam_r, fprime = loss.grad_gam, args = (x_r,), 
+  ##                          maxiter = n_inner, disp = 1)
+  ##  gam_r = res_gam[0].copy()
+  #
+  #res_fix_1 = fmin_l_bfgs_b(loss.eval_x_first, x_0.ravel().astype(np.float64), 
+  #                          fprime = loss.grad_x, args = (gam.ravel().astype(np.float64),), 
   #                          maxiter = n_inner, disp = 1)
-  #  gam_r = res_gam[0].copy()
-
-  res_fix_1 = fmin_l_bfgs_b(loss.eval_x_first, x_0, fprime = loss.grad_x, args = (gam,), 
-                            maxiter = n_inner, disp = 0)
-  res_fix_2 = fmin_l_bfgs_b(loss.eval_gam_first, gam_0, fprime = loss.grad_gam, args = (x,), 
-                            maxiter = n_inner, disp = 0)
-  res_fix_3 = fmin_l_bfgs_b(loss.eval_gam_first, 1.2*gam, fprime = loss.grad_gam, args = (x,), 
-                            maxiter = n_inner, disp = 0)
+  #res_fix_2 = fmin_l_bfgs_b(loss.eval_gam_first, gam_0, fprime = loss.grad_gam, args = (x,), 
+  #                          maxiter = n_inner, disp = 0)
+  #res_fix_3 = fmin_l_bfgs_b(loss.eval_gam_first, 1.2*gam, fprime = loss.grad_gam, args = (x,), 
+  #                          maxiter = n_inner, disp = 0)
