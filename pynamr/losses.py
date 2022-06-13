implement correct Gam gradient of Data fidelity loss

pynamr/losses.py
@@ -65,12 +65,12 @@ if __name__ == '__main__':
   n_ds = data_shape[0] 
   n    = ds*n_ds
 
-  tmp = gaussian_filter(np.pad(np.ones((n//2,n//2,n//2), dtype = np.float64), n//4),2)
-  x   = np.stack([tmp,tmp], axis = -1)
+  x   = np.stack([np.random.randn(n,n,n),np.random.randn(n,n,n)], axis = -1)
 
-  sens = 1e-2*(np.ones((ncoils,n_ds,n_ds,n_ds)).astype(np.float64) + 1j*np.ones((ncoils,n_ds,n_ds,n_ds)).astype(np.float64))
+  sens = np.random.rand(ncoils,n_ds,n_ds,n_ds) + 1j*np.random.rand(ncoils,n_ds,n_ds,n_ds)
+  sens *= 1e-2
  
-  gam = 0.9*np.ones((n,n,n)).astype(np.float64)
+  gam = np.random.rand(n,n,n)
   
   fwd_model = MonoExpDualTESodiumAcqModel(data_shape, ds, ncoils, sens, dt, xp)
   
@@ -82,8 +82,8 @@ if __name__ == '__main__':
   loss = DataFidelityLoss(fwd_model, data)
 
   # inital values
-  x_0   = 1.1*x
-  gam_0 = 0.7*gam
+  x_0   = np.random.rand(*x.shape)
+  gam_0 = np.random.rand(*gam.shape)
 
   # check gradients
   ll = loss.eval_x_first(x_0, gam_0)
@@ -92,16 +92,23 @@ if __name__ == '__main__':
 
   eps = 1e-6
 
-  delta_x = np.zeros(x.shape)
-  delta_x[64,64,64,0] = eps
-  print(gx[64,64,64,0], (loss.eval_x_first(x_0 + delta_x, gam_0) - ll) / eps)
+  vox_nums = [40,51,63]
 
+  for i in vox_nums:
+    delta_x = np.zeros(x.shape)
+    delta_x[i,i,i,0] = eps
+    print(gx[i,i,i,0], (loss.eval_x_first(x_0 + delta_x, gam_0) - ll) / eps)
 
-  for i in [40,51,63]:
+    delta_x = np.zeros(x.shape)
+    delta_x[i,i,i,1] = eps
+    print(gx[i,i,i,1], (loss.eval_x_first(x_0 + delta_x, gam_0) - ll) / eps)
+
+  print('')
+
+  for i in vox_nums:
     delta_g = np.zeros(gam.shape)
     delta_g[i,i,i] = eps
     print(gg[i,i,i], (loss.eval_x_first(x_0, gam_0 + delta_g) - ll) / eps)
-
 
 
 

pynamr/models.py
@@ -395,26 +395,35 @@ class MonoExpDualTESodiumAcqModel:
     # create a complex view of the input real input array with two channels
     F  = np.squeeze(F.astype(np.float64).view(dtype = np.complex128), axis = -1)
 
+    img = np.squeeze(img.view(dtype = np.complex128), axis = -1)
+
     #----------------------
     # send F, Gam to GPU
     if self._xp.__name__ == 'cupy':
       F   = self._xp.asarray(F)
       Gam = self._xp.asarray(Gam)
+      img = self._xp.asarray(img)
 
     f_ds = self._xp.zeros(F.shape[2:], dtype = self._xp.complex128)
 
     Gam_ds = downsample(downsample(downsample(Gam, self._ds, self._xp, axis = 0), 
                         self._ds, self._xp, axis = 1), self._ds, self._xp, axis = 2)
 
+    img_ds = downsample(downsample(downsample(img, self._ds, self._xp, axis = 0), 
+                        self._ds, self._xp, axis = 1), self._ds, self._xp, axis = 2)
+
+
     for i_sens in range(self._ncoils):
       for it in range(self._n_readout_bins):
+        n =  self._tr[it]/self._dt
+
         tmp0 = self._xp.zeros(F[i_sens,0,...].shape, dtype = F.dtype)
         tmp0[self._readout_inds[it]] = F[i_sens,0,...][self._readout_inds[it]]
-        f_ds += (self._tr[it]/self._dt) * (Gam_ds**((self._tr[it]/self._dt) - 1)) * self._xp.conj(self.sens[i_sens])*self._xp.fft.ifftn(tmp0, norm = 'ortho')
+        f_ds += n * (Gam_ds**(n - 1)) * self._xp.conj(img_ds * self.sens[i_sens])*self._xp.fft.ifftn(tmp0, norm = 'ortho')
 
         tmp1 = self._xp.zeros(F[i_sens,1,...].shape, dtype = F.dtype)
         tmp1[self._readout_inds[it]] = F[i_sens,1,...][self._readout_inds[it]]
-        f_ds += ((self._tr[it]/self._dt) + 1) * (Gam_ds**(self._tr[it]/self._dt)) * self._xp.conj(self.sens[i_sens])*self._xp.fft.ifftn(tmp1, norm = 'ortho')
+        f_ds += (n + 1) * (Gam_ds**n) * self._xp.conj(img_ds * self.sens[i_sens])*self._xp.fft.ifftn(tmp1, norm = 'ortho')
 
     # upsample f
     f = upsample(upsample(upsample(f_ds, self._ds, self._xp, axis = 0), self._ds, self._xp, axis = 1), 
@@ -428,10 +437,7 @@ class MonoExpDualTESodiumAcqModel:
     # convert complex128 arrays back to 2 float64 array
     f = np.stack([f.real, f.imag], axis = -1)
 
-    # multiply by the curent image
-    f *= img
-
-    return f.sum(-1)
+    return f[...,0]
 
 
   #------------------------------------------------------------------------------