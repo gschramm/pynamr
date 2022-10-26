890f667d2d6813faf8c07d01c11ffb3ec755d855
mfilip0 <marina.filipovicpierucci@kuleuven.be>
2022-10-26 11:56:53 +0200

    Improved recon_realdata: saving results or loading and displaying previous results, support for different exams, new input/output folder structure

--- scripts/recon_realdata.py
+++ scripts/recon_realdata.py
@@ -38,6 +38,7 @@ import pynamr
 
 parser = ArgumentParser(description = '3D real Na MRI dual echo reconstruction')
 parser.add_argument('case')
+parser.add_argument('--pathology',  default = 'TBI', choices = ['TBI','CSF'])
 parser.add_argument('--sdir',  default = '/uz/data/Admin/ngeworkingresearch/MarinaFilipovic/SodiumMRIdata/')
 parser.add_argument('--niter',  default = 20, type = int, help='number of optimization iterations')
 parser.add_argument('--n_outer', default = 10, type = int, help='number of outer optimization iterations if multistep')
@@ -46,10 +47,6 @@ parser.add_argument('--beta_gam', default = 1e-2, type = float, help='penalty st
 parser.add_argument('--n', default = 128, type = int, choices = [128,256], help='image size')
 parser.add_argument('--n_readout_bins', default = 16, type = int, help='TPI readout bins')
 parser.add_argument('--nnearest', default = 13,  type = int, help='Bowsher number of most similar voxels')
-parser.add_argument('--delta_t', default = 4.5, type = float, help='Time between TE1 and TE2 acquisition')
-parser.add_argument('--te1', default = 0.5, type = float, help='TE1, start of the first acquisition')
-parser.add_argument('--ncoils',   default = 1, type = int, help='number of coils')
-parser.add_argument('--data_n',   default = 64, type = int, help='data size')
 parser.add_argument('--instant_tpi_recon',   default = False, action='store_true', help='TPI readout instantaneous for reconstruction')
 parser.add_argument('--model_recon',   default = 'monoexp', type = str, choices = ['monoexp','fixedcomp','custom'],
                        help='forward model for reconstructing the raw data')
@@ -60,8 +57,9 @@ parser.add_argument('--t2bi_frac_l', default = 0.4, type = float, help='fixed T2
 parser.add_argument('--t2bi_smap_filename', default = None, type = str, help='T2* biexponential short component spatial map')
 parser.add_argument('--t2bi_lmap_filename', default = None, type = str, help='T2* biexponential long component spatial map')
 parser.add_argument('--t2mono_map_filename', default = None, type = str, help='T2* monoexponential (long) component spatial map (i.e. fluid)')
-parser.add_argument('--highres_mri_name', default = 'mprage')
-parser.add_argument('--no_highres_prior', action = 'store_true')
+parser.add_argument('--highres_mri_name', default = 'mprage', help='higher resolution mri name')
+parser.add_argument('--no_highres_prior', action = 'store_true', help='no additional higher resolution mri')
+parser.add_argument('--load_results', action = 'store_true', help='load already computed results, display and exit')
 
 
 args = parser.parse_args()
@@ -73,10 +71,6 @@ beta_gam    = args.beta_gam
 n           = args.n
 n_readout_bins     = args.n_readout_bins
 nnearest    = args.nnearest
-ncoils      = args.ncoils
-delta_t     = args.delta_t
-te1         = args.te1
-data_n      = args.data_n
 instant_tpi_recon = args.instant_tpi_recon
 model_recon = args.model_recon
 t2bi_s      = args.t2bi_s
@@ -90,11 +84,109 @@ highres_mri_name = args.highres_mri_name
 no_highres_prior = args.no_highres_prior
 sdir        = args.sdir
 case        = args.case
+pathology  = args.pathology
+load_results = args.load_results
+
 
 #-------------------------------------------------------------------------------------
-# Initialize some parameters
+# Utility functions for results
 #-------------------------------------------------------------------------------------
 
+# display reconstruction results
+def display_results():
+    if model_recon=="monoexp":
+        ims_1 = dict(cmap=plt.cm.viridis, vmin = 0, vmax = std_te1.max())
+        ims_2 = dict(cmap=plt.cm.viridis, vmin = 0, vmax = 1.)
+        t1_1 = dict(cmap=plt.cm.gray, vmin = 0, vmax = highres_proton_img.max())
+
+        # empirical rough estimate of gamma for TBI data
+        gam_rough_est = np.divide(std_te2, std_te1, where=std_te1>0.1*std_te1.max())
+        gam_rough_est[std_te1<=0.1*std_te1.max()]=1.
+
+        # reconstructed image at time 0 and simple recon at TE1
+        vi_x_te1 = pv.ThreeAxisViewer([std_te1,
+                         np.linalg.norm(x_r, axis=-1),
+                         highres_proton_img,
+                         gam_r],
+                         imshow_kwargs=[ims_1, ims_1, t1_1, ims_2])
+
+        # reconstructed Gamma and a rough estimation from simple recons
+        vi_gam = pv.ThreeAxisViewer([gam_rough_est,
+                         gam_r],
+                         imshow_kwargs=[ims_2, ims_2])
+
+        # reconstructed and simple TE1 and TE2 images
+        vi_te1_te2 = pv.ThreeAxisViewer([std_te1,
+                         np.linalg.norm(x_r, axis=-1)*gam_r**(te1/delta_t),
+                         std_te2,
+                         np.linalg.norm(x_r, axis=-1)*gam_r**(1+(te1/delta_t))],
+                         imshow_kwargs=[ims_1, ims_1, ims_1, ims_1])
+    elif model_recon=="fixedcomp":
+        ims_1 = dict(cmap=plt.cm.viridis, vmin = 0, vmax = std_te1.max())
+
+        vi = pv.ThreeAxisViewer([np.linalg.norm(x_r[0], axis=-1),
+                         np.linalg.norm(x_r[1], axis=-1),
+                         std_te1_filtered],
+                         imshow_kwargs=[ims_1, ims_1, ims_1])
+    else:
+        raise NotImplementedError
+
+# save reconstruction results to files
+def save_results():
+    # write input arguments to file
+    with open(os.path.join(odir,'input_params.csv'), 'w') as f:
+        for x in args.__dict__.items():
+            f.write("%s,%s\n"%(x[0],x[1]))
+    # write images
+    if model_recon=="monoexp":
+        x_r.tofile(os.path.join(odir,'x_r.img'))
+        gam_r.tofile(os.path.join(odir,'gam_r.img'))
+    elif model_recon=="fixedcomp":
+        x_r.tofile(os.path.join(odir,'x_r.img'))
+    else:
+        raise NotImplementedError
+
+
+#-------------------------------------------------------------------------------------
+# Load and preprocess "raw" data
+#-------------------------------------------------------------------------------------
+
+# input folders and specific parameters
+if pathology=='TBI':
+    casedir = f"TBI-0{args.case}"
+    sdir1 = os.path.join(args.sdir, casedir, 'TE03', 'DeNoise')
+    sdir2 = os.path.join(args.sdir, casedir, 'TE5', 'DeNoise')
+    fpattern = '*.c?'
+    ncoils = 8
+    data_n = 64
+    te1 = 0.3
+    delta_t = 4.7
+elif pathology=='CSF':
+    casedir = f"CSF_H0{args.case}b1_birdcage"
+    sdir1 = os.path.join(args.sdir, casedir, 'TE05','DeNoise')
+    sdir2 = os.path.join(args.sdir, casedir, 'TE5', 'DeNoise')
+    fpattern = '*.c?'
+    ncoils  = 1
+    data_n = 64
+    te1 = 0.5
+    delta_t = 4.5
+
+# find files
+fnames1 = sorted(glob(os.path.join(sdir1, fpattern)))
+fnames2 = sorted(glob(os.path.join(sdir2, fpattern)))
+if (len(fnames1) != ncoils) or (len(fnames2) != ncoils):
+    raise ValueError('The number of data files is not correct')
+odir = os.path.join(sdir, casedir, f'betax_{beta_x:.1E}'+ (f'_betagam_{beta_gam:.1E}' if model_recon=='monoexp' else '')+
+                                   (f'_{highres_mri_name}_nbow_{nnearest}' if not no_highres_prior else ''))
+if not os.path.exists(odir):
+  os.makedirs(odir)
+
+# high resolution registered proton MRI image
+if not no_highres_prior:
+    highres_proton_img_file = os.path.join(sdir, casedir, f"proton/{highres_mri_name}_n4_aligned_128.npy")
+
+
+# initialize some parameters and variables
 # 2 TE values currently
 nechos = 2
 # shape of the kspace data of a single coil
@@ -103,37 +195,6 @@ data_shape = (data_n, data_n, data_n)
 recon_shape = (n, n, n)
 # down sample factor (recon cube size / data cube size), must be integer
 ds = n//data_n
-
-
-#-------------------------------------------------------------------------------------
-# Load and preprocess "raw" data
-#-------------------------------------------------------------------------------------
-
-# raw data
-sdir1 = os.path.join(args.sdir, f"TBI-0{args.case}",f"TBI-0{args.case}_TE03", 'DeNoise', 'DeNoise_kw0')
-sdir2 = os.path.join(args.sdir, f"TBI-0{args.case}",f"TBI-0{args.case}_TE5", 'DeNoise', 'DeNoise_kw0')
-fpattern = '*.c?'
-fnames1 = sorted(glob(os.path.join(sdir1, fpattern)))
-fnames2 = sorted(glob(os.path.join(sdir2, fpattern)))
-if (len(fnames1) != 8) or (len(fnames2) != 8):
-  raise ValueError('Not enough data files found')
-ncoils  = len(fnames1)
-
-# output folder
-if highres_mri_name and not no_highres_prior:
-  odir  = os.path.join(sdir, f'{datetime.now().strftime("%y%m%d-%H%M%S")}__br_{beta_x:.1E}__bg_{beta_gam:.1E}__ne_{nnearest}')
-else:
-  odir  = os.path.join(sdir, f'{datetime.now().strftime("%y%m%d-%H%M%S")}__br_{beta_x:.1E}__bg_{beta_gam:.1E}__no_anat')
-if not os.path.exists(odir):
-  os.makedirs(odir)
-# write input arguments to file
-with open(os.path.join(odir,'input_params.csv'), 'w') as f:
-  for x in args.__dict__.items():
-    f.write("%s,%s\n"%(x[0],x[1]))
-
-# high resolution image prior
-t1_vol = np.load(os.path.join(sdir,f"TBI-0{args.case}", f"{highres_mri_name}_proton/T1_n4_aligned_128.nii.npy"))
-
 # initialize variables
 data           = np.zeros((ncoils, nechos) + data_shape,  dtype = np.complex64)
 data_pad       = np.zeros((ncoils, nechos) + recon_shape,  dtype = np.complex64)
@@ -180,8 +241,13 @@ for i in range(ncoils):
     sens[i,...]   = coil_im / sos_filt_te1
 
 # scale signal to be not too far away from 1
-scale_factor = np.median(sos_filt_te1)
+scale_factor = np.max(sos_filt_te1)
 data = data / scale_factor
+
+# higher res proton image
+if not no_highres_prior:
+    highres_proton_img = np.load(highres_proton_img_file)
+
 
 #-------------------------------------------------------------------------------------
 # "Standard/simple" recon
@@ -194,6 +260,22 @@ std_te2 = pynamr.sum_of_squares_reconstruction(data_pad[:,1,...], complex_format
 # filter the SOS images
 std_te1_filtered = gaussian_filter(std_te1, 1.)
 std_te2_filtered = gaussian_filter(std_te2, 1.)
+
+
+#-------------------------------------------------------------------------------------
+# Load and display already computed reconstruction results and exit
+#-------------------------------------------------------------------------------------
+
+if load_results:
+    if model_recon == "monoexp":
+        x_r = np.fromfile(os.path.join(odir,'x_r.img'), dtype=np.float64).reshape(recon_shape+(2,))
+        gam_r = np.fromfile(os.path.join(odir,'gam_r.img'), dtype=np.float64).reshape(recon_shape)
+        display_results()
+    elif model_recon == "fixedcomp":
+        x_r = np.fromfile(os.path.join(odir,'x_r.img'), dtype=np.float64).reshape((2,)+recon_shape+(2,))
+        display_results()
+    sys.exit()
+
 
 #-------------------------------------------------------------------------------------
 # Reconstruction
@@ -229,7 +311,7 @@ data_fidelity_loss = pynamr.DataFidelityLoss(fwd_model, data)
 #-------------------------------------------------------------------------------------
 # setup the priors
 # simulate a perfect anatomical prior image (with changed contrast but matching edges)
-aimg = t1_vol.copy()
+aimg = highres_proton_img.copy()
 bowsher_loss = pynamr.generate_bowsher_loss(aimg, nnearest)
 
 #-------------------------------------------------------------------------------------
@@ -244,6 +326,11 @@ else:
     raise NotImplementedError
 
 loss = pynamr.TotalLoss(data_fidelity_loss, penalty_info, beta_info)
+
+
+#-------------------------------------------------------------------------------------
+# load existing results
+
 
 #-------------------------------------------------------------------------------------
 # run the recons
@@ -285,33 +372,11 @@ if model_recon=="monoexp":
     x_r = unknowns[pynamr.VarName.IMAGE].value * scale_factor
     gam_r = unknowns[pynamr.VarName.GAMMA].value
 
+    # save the results
+    save_results()
+
     # show the results
-    ims_1 = dict(cmap=plt.cm.viridis, vmin = 0, vmax = std_te1.max())
-    ims_2 = dict(cmap=plt.cm.viridis, vmin = 0, vmax = 1.)
-    t1_1 = dict(cmap=plt.cm.gray, vmin = 0, vmax = t1_vol.max())
-
-    # empirical rough estimate of gamma for TBI data
-    gam_rough_est = np.divide(std_te2, std_te1, where=std_te1>0.1*std_te1.max())
-    gam_rough_est[std_te1<=0.1*std_te1]=1.
-
-    # reconstructed image at time 0 and simple recon at TE1
-    vi_x_te1 = pv.ThreeAxisViewer([std_te1,
-                         np.linalg.norm(x_r, axis=-1),
-                         t1_vol,
-                         gam_r],
-                         imshow_kwargs=[ims_1, ims_1, t1_1, ims_2])
-
-    # reconstructed Gamma and a rough estimation from simple recons
-    vi_gam = pv.ThreeAxisViewer([gam_rough_est,
-                         gam_r],
-                         imshow_kwargs=[ims_2, ims_2])
-
-    # reconstructed and simple TE1 and TE2 images
-    vi_te1_te2 = pv.ThreeAxisViewer([std_te1,
-                         np.linalg.norm(x_r, axis=-1)*gam_r**(te1/delta_t),
-                         std_te2,
-                         np.linalg.norm(x_r, axis=-1)*gam_r**(1+(te1/delta_t))],
-                         imshow_kwargs=[ims_1, ims_1, ims_1, ims_1])
+    display_results()
 
 elif model_recon=="fixedcomp":
     # allocate arrays for recons and copy over initial values
@@ -325,18 +390,17 @@ elif model_recon=="fixedcomp":
                           maxiter=niter,
                           disp=1)
 
-    # postprocess results
+    # postprocess the results
     unknowns[pynamr.VarName.PARAM].value = res_1[0].copy().reshape(unknowns[pynamr.VarName.PARAM].shape)
-    x_r = unknowns[pynamr.VarName.PARAM] * scale_factor
+    x_r = unknowns[pynamr.VarName.PARAM].value * scale_factor
 
+    # save the results
+    save_results()
 
     # show the results
-    ims_1 = dict(cmap=plt.cm.viridis, vmin = 0, vmax = std_te1.max())
+    display_results()
 
-    vi = pv.ThreeAxisViewer([np.linalg.norm(x_r[0], axis=-1),
-                         np.linalg.norm(x_r[1], axis=-1)],
-                         std_te1_filtered,
-                         imshow_kwargs=[ims_1, ims_1, ims_1])
 else:
     raise NotImplementedError
+
 
