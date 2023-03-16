61dd8e8c453553bb490a04a1aa6f5b0d209963e2
Georg <gschra2@stanford.edu>
2023-03-16 11:13:13 -0700

    add better analysis script for sigpy brainweb simulations

--- scripts/show_sigpy_brainweb.py
+++ scripts/show_sigpy_brainweb.py
@@ -1,189 +1,105 @@
 import argparse
+import json
 from pathlib import Path
 import numpy as np
 import pymirc.viewer as pv
+import nibabel as nib
 from pymirc.image_operations import zoom3d
 from scipy.ndimage import binary_erosion
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--regularization_norm',
+parser.add_argument('--max_num_iter', type=int, default=500)
+parser.add_argument('--noise_level', type=float, default=2e-2)
+parser.add_argument('--phantom',
                     type=str,
-                    default='L1',
-                    choices=['L1', 'L2'])
-parser.add_argument('--beta', type=float, default=2e-2)
-parser.add_argument('--max_num_iter', type=int, default=1000)
-parser.add_argument('--noise_level', type=float, default=3e-2)
-parser.add_argument('--seed', type=int, default=1)
+                    default='brainweb',
+                    choices=['brainweb', 'blob'])
+parser.add_argument('--no_decay', action='store_true')
 args = parser.parse_args()
 
-regularization_norm = args.regularization_norm
-beta = args.beta
 max_num_iter = args.max_num_iter
 noise_level = args.noise_level
-seed = args.seed
+phantom = args.phantom
+no_decay = args.no_decay
 
-regularization_norm_non_anatomical = 'L2'
-beta_non_anatomical = 2e-1
-short_fraction = 0.6
-ishape = (128, 128, 128)
+#-----------------------------------------------------------------------
 
-zoomfac = 256 / ishape[0]
+sim_shape = (160, 160, 160)
+iter_shape = (128, 128, 128)
+grid_shape = (64, 64, 64)
 
-#---------------------------------------------------------------
+field_of_view_cm: float = 22.
 
-odir = Path('run') / f'i_{max_num_iter:04}_nl_{noise_level:.1E}_s_{seed:03}'
+odirs = sorted(
+    list(
+        Path('run_brainweb').glob(
+            f'{phantom}_nodecay_{no_decay}_i_{max_num_iter:04}_nl_{noise_level:.1E}_s_*'
+        )))
 
-x = np.abs(np.load(odir / 'na_gt.npy'))
-t1_image = np.load(odir / 't1.npy')
-true_ratio_image_short = np.load(odir / 'true_ratio_short.npy')
-true_ratio_image_long = np.load(odir / 'true_ratio_long.npy')
+#-----------------------------------------------------------------------
+# load the brainweb label array
 
-x = zoom3d(x, 256 / x.shape[0])
-t1_image = zoom3d(t1_image, 256 / t1_image.shape[0])
-true_ratio_image_short = zoom3d(true_ratio_image_short,
-                                256 / true_ratio_image_short.shape[0])
-true_ratio_image_long = zoom3d(true_ratio_image_long,
-                               256 / true_ratio_image_long.shape[0])
+with open('.simulation_config.json', 'r') as f:
+    data_root_dir: str = json.load(f)['data_root_dir']
 
-ifft1 = zoom3d(np.abs(np.load(odir / 'ifft1.npy')), zoomfac)
-ifft2 = zoom3d(np.abs(np.load(odir / 'ifft2.npy')), zoomfac)
-ifft1_filt = zoom3d(np.abs(np.load(odir / 'ifft1_filt.npy')), zoomfac)
-ifft2_filt = zoom3d(np.abs(np.load(odir / 'ifft2_filt.npy')), zoomfac)
+phantom_data_path: Path = Path(data_root_dir) / 'brainweb54'
 
-outfile1 = odir / f'recon_echo_1_no_decay_model_{regularization_norm_non_anatomical}_{beta_non_anatomical:.1E}.npz'
-d1 = np.load(outfile1)
-recon_echo_1_wo_decay_model = zoom3d(np.abs(d1['x']), zoomfac)
+label_nii = nib.as_closest_canonical(
+    nib.load(phantom_data_path / 'subject54_crisp_v.nii'))
 
-outfile2 = odir / f'recon_echo_2_no_decay_model_{regularization_norm_non_anatomical}_{beta_non_anatomical:.1E}.npz'
-d2 = np.load(outfile2)
-recon_echo_2_wo_decay_model = zoom3d(np.abs(d2['x']), zoomfac)
+# pad to 220mm FOV
+lab_voxelsize = label_nii.header['pixdim'][1]
+lab = label_nii.get_fdata()
+pad_size_220 = ((220 - np.array(lab.shape) * lab_voxelsize) / lab_voxelsize /
+                2).astype(int)
+pad_size_220 = ((pad_size_220[0], pad_size_220[0]),
+                (pad_size_220[1], pad_size_220[1]), (pad_size_220[2],
+                                                     pad_size_220[2]))
+lab = np.pad(lab, pad_size_220, 'constant')
 
-outfile1t = odir / f'recon_echo_1_true_decay_model_{regularization_norm_non_anatomical}_{beta_non_anatomical:.1E}.npz'
-d1t = np.load(outfile1t)
-recon_echo_1_true_decay_model = zoom3d(np.abs(d1t['x']), zoomfac)
+# load the aparc parcelation
+aparc_nii = nib.as_closest_canonical(
+    nib.load(phantom_data_path / 'aparc.DKTatlas+aseg_native.nii.gz'))
+aparc = np.pad(np.asanyarray(aparc_nii.dataobj), pad_size_220, 'constant')
 
-outfile2t = odir / f'recon_echo_2_true_decay_model_{regularization_norm_non_anatomical}_{beta_non_anatomical:.1E}.npz'
-d2t = np.load(outfile2t)
-recon_echo_2_true_decay_model = zoom3d(np.abs(d2t['x']), zoomfac)
+roi_inds = {}
+roi_inds['cortical_gm'] = np.where(aparc >= 1000)
+roi_inds['putamen'] = np.where(np.isin(aparc, [12, 51]))
+roi_inds['wm'] = np.where(binary_erosion(lab == 3, iterations=5))
+#-----------------------------------------------------------------------
 
-outfile3 = odir / f'agr_echo_1_no_decay_model_{regularization_norm}_{beta:.1E}.npz'
-d3 = np.load(outfile3)
-agr_echo_1_wo_decay_model = zoom3d(np.abs(d3['x']), zoomfac)
+beta_non_anatomical = 3e-1
+beta_anatomical = 1e-2
 
-outfile4 = odir / f'agr_echo_2_no_decay_model_{regularization_norm}_{beta:.1E}.npz'
-d4 = np.load(outfile4)
-agr_echo_2_wo_decay_model = zoom3d(np.abs(d4['x']), zoomfac)
+recons_e1_no_decay = np.zeros((len(odirs), 440, 440, 440))
+agrs_e1_no_decay = np.zeros((len(odirs), 440, 440, 440))
+agrs_both_echos_w_decay_model = np.zeros((len(odirs), 440, 440, 440))
 
-outfile5 = odir / f'agr_echo_1_true_decay_model_{regularization_norm}_{beta:.1E}.npz'
-d5 = np.load(outfile5)
-agr_echo_1_true_decay_model = zoom3d(np.abs(d5['x']), zoomfac)
+for i, odir in enumerate(odirs):
+    print(odir)
+    ofile_e1_no_decay = odir / f'recon_echo_1_no_decay_model_{max_num_iter}_{beta_non_anatomical}.npz'
+    ofile_e1_no_decay_agr = odir / f'agr_echo_1_no_decay_model_{max_num_iter}_{beta_anatomical}.npz'
+    ofile_agr_both_echos = odir / f'agr_both_echos_w_decay_model_{beta_anatomical:.1E}.npz'
 
-outfile6 = odir / f'agr_echo_2_true_decay_model_{regularization_norm}_{beta:.1E}.npz'
-d6 = np.load(outfile6)
-agr_echo_2_true_decay_model = zoom3d(np.abs(d6['x']), zoomfac)
+    d = np.load(ofile_e1_no_decay)
+    recons_e1_no_decay[i, ...] = zoom3d(np.abs(d['x']), 440 / iter_shape[0])
 
-outfile7 = odir / f'agr_echo_1_est_decay_model_{regularization_norm}_{beta:.1E}.npz'
-d7 = np.load(outfile7)
-agr_echo_1_est_decay_model = zoom3d(np.abs(d7['x']), zoomfac)
+    da = np.load(ofile_e1_no_decay_agr)
+    agrs_e1_no_decay[i, ...] = zoom3d(np.abs(da['x']), 440 / iter_shape[0])
 
-outfile8 = odir / f'agr_echo_2_est_decay_model_{regularization_norm}_{beta:.1E}.npz'
-d8 = np.load(outfile8)
-agr_echo_2_est_decay_model = zoom3d(np.abs(d8['x']), zoomfac)
+    db = np.load(ofile_agr_both_echos)
+    agrs_both_echos_w_decay_model[i, ...] = zoom3d(np.abs(db['x']),
+                                                   440 / iter_shape[0])
 
-est_ratio = zoom3d(np.load(odir / 'est_ratio.npy'), zoomfac)
+# calculate the ROI averages
+recon_e1_no_decay_roi_means = {}
+agr_e1_no_decay_roi_means = {}
+agr_both_echos_w_decay_roi_means = {}
 
-#--------------------------------------------------------------------------------------
-gm = np.load(odir.parent / 'gm_256.npy')
-wm = np.load(odir.parent / 'wm_256.npy')
-
-wm_mask = binary_erosion(np.abs(x * wm - 1) < 0.01, iterations=2)
-
-wm_inds = np.where(wm_mask)
-gm_inds = np.where(gm)
-
-gmwm_ratio_non_anatomical = recon_echo_1_wo_decay_model[gm_inds].mean(
-) / recon_echo_1_wo_decay_model[wm_inds].mean()
-
-gmwm_ratio_non_anatomical_true_decay_model = recon_echo_1_true_decay_model[
-    gm_inds].mean() / recon_echo_1_true_decay_model[wm_inds].mean()
-
-gmwm_ratio_anatomical_wo_decay_model = agr_echo_1_wo_decay_model[gm_inds].mean(
-) / agr_echo_1_wo_decay_model[wm_inds].mean()
-
-gmwm_ratio_anatomical_true_decay_model = agr_echo_1_true_decay_model[
-    gm_inds].mean() / agr_echo_1_true_decay_model[wm_inds].mean()
-
-gmwm_ratio_anatomical_est_decay_model = agr_echo_1_est_decay_model[
-    gm_inds].mean() / agr_echo_1_est_decay_model[wm_inds].mean()
-
-gmwm_ratio_true = x[gm_inds].mean() / x[wm_inds].mean()
-
-#--------------------------------------------------------------------------------------
-# approximate "true" monoexp. ratio
-true_ratio = short_fraction * true_ratio_image_short + (
-    1 - short_fraction) * true_ratio_image_long
-
-ims = 4 * [dict(vmin=0, vmax=3.5, cmap='Greys_r')]
-kwargs = dict(sl_z=127)
-
-vi1 = pv.ThreeAxisViewer(
-    [
-        np.flip(x, (0, 1)) for x in [
-            np.abs(ifft1),
-            np.abs(ifft1_filt),
-            np.abs(recon_echo_1_wo_decay_model),
-            np.abs(recon_echo_1_true_decay_model),
-        ]
-    ],
-    imshow_kwargs=ims,
-    rowlabels=[
-        '', '', f'GM/WM = {gmwm_ratio_non_anatomical:.2f}',
-        f'GM/WM = {gmwm_ratio_non_anatomical_true_decay_model:.2f}'
-    ],
-    **kwargs)
-
-vi2 = pv.ThreeAxisViewer(
-    [
-        np.flip(x, (0, 1)) for x in [
-            np.abs(agr_echo_1_wo_decay_model),
-            np.abs(agr_echo_1_true_decay_model),
-            np.abs(agr_echo_1_est_decay_model),
-        ]
-    ],
-    imshow_kwargs=ims,
-    rowlabels=[
-        f'GM/WM = {gmwm_ratio_anatomical_wo_decay_model:.2f}',
-        f'GM/WM = {gmwm_ratio_anatomical_true_decay_model:.2f}',
-        f'GM/WM = {gmwm_ratio_anatomical_est_decay_model:.2f}'
-    ],
-    **kwargs)
-
-vi3 = pv.ThreeAxisViewer([
-    np.flip(x, (0, 1)) for x in [
-        np.abs(ifft2),
-        np.abs(ifft2_filt),
-        np.abs(recon_echo_2_wo_decay_model),
-        np.abs(recon_echo_2_true_decay_model),
-    ]
-],
-                         imshow_kwargs=ims,
-                         **kwargs)
-
-vi4 = pv.ThreeAxisViewer([
-    np.flip(x, (0, 1)) for x in [
-        np.abs(agr_echo_2_wo_decay_model),
-        np.abs(agr_echo_2_true_decay_model),
-        np.abs(agr_echo_2_est_decay_model),
-    ]
-],
-                         imshow_kwargs=ims,
-                         **kwargs)
-
-vi5 = pv.ThreeAxisViewer([np.flip(x, (0, 1)) for x in [true_ratio, est_ratio]],
-                         imshow_kwargs=dict(vmin=0, vmax=1, cmap='Greys_r'),
-                         **kwargs)
-
-vi6 = pv.ThreeAxisViewer([np.flip(x, (0, 1)) for x in [x, 0.003 * t1_image]],
-                         imshow_kwargs=dict(vmin=0, vmax=3.5, cmap='Greys_r'),
-                         rowlabels=[f'GM/WM = {gmwm_ratio_true:.2f}', ''],
-                         **kwargs)
+for key, inds in roi_inds.items():
+    recon_e1_no_decay_roi_means[key] = np.array(
+        [x[inds].mean() for x in recons_e1_no_decay])
+    agr_e1_no_decay_roi_means[key] = np.array(
+        [x[inds].mean() for x in agrs_e1_no_decay])
+    agr_both_echos_w_decay_roi_means[key] = np.array(
+        [x[inds].mean() for x in agrs_both_echos_w_decay_model])
