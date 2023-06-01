bcacc87acc4d744db00ded3129d3b3d369cde5a4
Georg <gschra2@stanford.edu>
2023-06-01 15:44:40 -0700

    improve siemens plots

--- sigpy/analyze_siemens.py
+++ sigpy/analyze_siemens.py
@@ -9,8 +9,6 @@ from collections import OrderedDict
 from pathlib import Path
 from utils import numpy_volume_to_sitk_image, sitk_image_to_numpy_volume
 
-import argparse
-
 
 def resample_sodium_to_t1_grid(na_image,
                                na_voxsize,
@@ -33,136 +31,220 @@ def resample_sodium_to_t1_grid(na_image,
 
 
 #-----------------------------------------------------------------------------
+q = []
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--pnum', type=int, default=31)
-args = parser.parse_args()
-
-pnum = args.pnum
-
+pdict = OrderedDict({'31': [96, 134], '32': [78, 123], '37': [99, 131]})
 sdir = 'recons_128'
 agr_w_decay_file = 'agr_both_echo_w_decay_model_L1_3.0E-03_3.0E-01_2000_19.npz'
 agr_wo_decay_file = 'agr_echo_1_no_decay_model_L1_3.0E-03_2000.npz'
 ratio_file = 'est_ratio_3.0E-03_3.0E-01_100_19.npy'
 conv_file = 'recon_echo_1_no_decay_model_L2_1.0E-01_2000.npz'
 
-pdir = Path(f'/data/sodium_mr/NYU/CSF{pnum}_raw')
+dfs = OrderedDict()
 
-# load the t1
-t1_nii = nib.as_closest_canonical(nib.load(pdir / 't1.nii'))
-t1 = t1_nii.get_fdata()
+plt.style.use('dark_background')
 
-t1_affine = t1_nii.affine
-t1_voxsize = t1_nii.header['pixdim'][1:4]
-t1_origin = t1_affine[:-1, -1]
+for ip, (pnum, [slz, sly]) in enumerate(pdict.items()):
 
-# load the freesurfer seg
-aparc_nii = nib.as_closest_canonical(nib.load(pdir / 'aparc+aseg-native.nii'))
-aparc = aparc_nii.get_fdata()
+    pdir = Path(f'/data/sodium_mr/NYU/CSF{pnum}_raw')
 
-# load the transform that aligns the T1 to the sodium recons
-na_origin = np.loadtxt(pdir / sdir / 'na_origin.txt')
-final_transform = sitk.ReadTransform(str(pdir / sdir / 't1_transform.tfm'))
+    # load the t1
+    t1_nii = nib.as_closest_canonical(nib.load(pdir / 't1.nii'))
+    t1 = t1_nii.get_fdata()
 
-# load the AGR including decay model
-na_voxsize = 10 * 22 / np.array([128, 128, 128])
-na_origin = t1_origin.copy()
+    t1_affine = t1_nii.affine
+    t1_voxsize = t1_nii.header['pixdim'][1:4]
+    t1_origin = t1_affine[:-1, -1]
 
-# load and resample the sodium recons
-conv = resample_sodium_to_t1_grid(
-    np.abs(np.load(pdir / sdir / conv_file)['x']), na_voxsize, na_origin, t1,
-    t1_voxsize, t1_origin, final_transform)
+    # load the freesurfer seg
+    aparc_nii = nib.as_closest_canonical(
+        nib.load(pdir / 'aparc+aseg-native.nii'))
+    aparc = aparc_nii.get_fdata()
 
-agr_wo_decay = resample_sodium_to_t1_grid(
-    np.abs(np.load(pdir / sdir / agr_wo_decay_file)['x']), na_voxsize,
-    na_origin, t1, t1_voxsize, t1_origin, final_transform)
+    # load the transform that aligns the T1 to the sodium recons
+    na_origin = np.loadtxt(pdir / sdir / 'na_origin.txt')
+    final_transform = sitk.ReadTransform(str(pdir / sdir / 't1_transform.tfm'))
 
-agr_w_decay = resample_sodium_to_t1_grid(
-    np.abs(np.load(pdir / sdir / agr_w_decay_file)['x']), na_voxsize,
-    na_origin, t1, t1_voxsize, t1_origin, final_transform)
+    # load the AGR including decay model
+    na_voxsize = 10 * 22 / np.array([128, 128, 128])
+    na_origin = t1_origin.copy()
 
-est_ratio = resample_sodium_to_t1_grid(np.abs(np.load(pdir / sdir /
-                                                      ratio_file)),
-                                       na_voxsize,
-                                       na_origin,
-                                       t1,
-                                       t1_voxsize,
-                                       t1_origin,
-                                       final_transform,
-                                       missing=1.0)
+    # load and resample the sodium recons
+    conv = resample_sodium_to_t1_grid(
+        np.abs(np.load(pdir / sdir / conv_file)['x']), na_voxsize, na_origin,
+        t1, t1_voxsize, t1_origin, final_transform)
 
+    agr_wo_decay = resample_sodium_to_t1_grid(
+        np.abs(np.load(pdir / sdir / agr_wo_decay_file)['x']), na_voxsize,
+        na_origin, t1, t1_voxsize, t1_origin, final_transform)
+
+    agr_w_decay = resample_sodium_to_t1_grid(
+        np.abs(np.load(pdir / sdir / agr_w_decay_file)['x']), na_voxsize,
+        na_origin, t1, t1_voxsize, t1_origin, final_transform)
+
+    est_ratio = resample_sodium_to_t1_grid(np.abs(
+        np.load(pdir / sdir / ratio_file)),
+                                           na_voxsize,
+                                           na_origin,
+                                           t1,
+                                           t1_voxsize,
+                                           t1_origin,
+                                           final_transform,
+                                           missing=1.0)
+
+    #------------------------------------------------------------------------------
+    # flip all images to LPS
+
+    t1 = np.flip(t1, (0, 1))
+    conv = np.flip(conv, (0, 1))
+    agr_wo_decay = np.flip(agr_wo_decay, (0, 1))
+    agr_w_decay = np.flip(agr_w_decay, (0, 1))
+    est_ratio = np.flip(est_ratio, (0, 1))
+    aparc = np.flip(aparc, (0, 1))
+
+    #------------------------------------------------------------------------------
+    # flip define ROIs and quantify
+
+    agr_wo_means = OrderedDict()
+    agr_w_means = OrderedDict()
+    conv_means = OrderedDict()
+
+    roi_inds = OrderedDict()
+    cortical_gm_mask = (aparc >= 1000).astype(np.uint8)
+    wm_mask = (aparc == 2).astype(np.uint8) + (aparc == 41).astype(np.uint8)
+    # WM next to GM
+    wm_gm_mask = binary_dilation(cortical_gm_mask, iterations=2) * wm_mask
+    central_wm_mask = binary_erosion(wm_mask, iterations=5)
+    ventricle_mask = (aparc == 4).astype(np.uint8) + (aparc == 43).astype(
+        np.uint8)
+
+    roi_inds['cortical GM'] = np.where(cortical_gm_mask)
+    roi_inds['WM'] = np.where(wm_mask)
+    #roi_inds['ventricles'] = np.where(ventricle_mask)
+
+    for roi, inds in roi_inds.items():
+        if ip == 0:
+            dfs[roi] = pd.DataFrame()
+
+        tmp = pd.DataFrame(
+            {
+                'conv': conv[inds].mean(),
+                'AGR wo decay': agr_wo_decay[inds].mean(),
+                'AGR w decay': agr_w_decay[inds].mean()
+            },
+            index=[pnum])
+
+        dfs[roi] = pd.concat((dfs[roi], tmp))
+
+    #------------------------------------------------------------------------------
+
+    bbox = find_objects(t1 > 0.1 * np.percentile(t1, 99.9))[0]
+
+    vmax = np.percentile(agr_w_decay, 99.99)
+    tmax = np.percentile(t1, 99.9)
+
+    fig, ax = plt.subplots(2, 5, figsize=(5 * 2, 2 * 2))
+
+    i0 = ax[0, 0].imshow(t1[bbox][:, :, slz].T,
+                         cmap='Greys_r',
+                         vmin=0,
+                         vmax=tmax)
+    ax[1, 0].imshow(t1[bbox][:, sly, :].T,
+                    origin='lower',
+                    cmap='Greys_r',
+                    vmin=0,
+                    vmax=tmax)
+    i1 = ax[0, 1].imshow(conv[bbox][:, :, slz].T,
+                         cmap='Greys_r',
+                         vmin=0,
+                         vmax=vmax)
+    ax[1, 1].imshow(conv[bbox][:, sly, :].T,
+                    origin='lower',
+                    cmap='Greys_r',
+                    vmin=0,
+                    vmax=vmax)
+    i2 = ax[0, 2].imshow(agr_wo_decay[bbox][:, :, slz].T,
+                         cmap='Greys_r',
+                         vmin=0,
+                         vmax=vmax)
+    ax[1, 2].imshow(agr_wo_decay[bbox][:, sly, :].T,
+                    origin='lower',
+                    cmap='Greys_r',
+                    vmin=0,
+                    vmax=vmax)
+    i3 = ax[0, 3].imshow(agr_w_decay[bbox][:, :, slz].T,
+                         cmap='Greys_r',
+                         vmin=0,
+                         vmax=vmax)
+    ax[1, 3].imshow(agr_w_decay[bbox][:, sly, :].T,
+                    origin='lower',
+                    cmap='Greys_r',
+                    vmin=0,
+                    vmax=vmax)
+
+    i4 = ax[0, 4].imshow(est_ratio[bbox][:, :, slz].T,
+                         cmap='Greys_r',
+                         vmin=0,
+                         vmax=1)
+    ax[1, 4].imshow(est_ratio[bbox][:, sly, :].T,
+                    origin='lower',
+                    cmap='Greys_r',
+                    vmin=0,
+                    vmax=1)
+
+    cax0 = fig.add_axes([0.02, 0.05, 0.18, 0.01])
+    cb0 = fig.colorbar(i0, cax=cax0, orientation='horizontal')
+    cb0.ax.tick_params(labelsize='small')
+    cax1 = fig.add_axes([0.02 + 1 * 0.196, 0.05, 0.18, 0.01])
+    cb1 = fig.colorbar(i1, cax=cax1, orientation='horizontal')
+    cb1.ax.tick_params(labelsize='small')
+    cax2 = fig.add_axes([0.02 + 2 * 0.196, 0.05, 0.18, 0.01])
+    cb2 = fig.colorbar(i2, cax=cax2, orientation='horizontal')
+    cb2.ax.tick_params(labelsize='small')
+    cax3 = fig.add_axes([0.02 + 3 * 0.196, 0.05, 0.18, 0.01])
+    cb3 = fig.colorbar(i3, cax=cax3, orientation='horizontal')
+    cb3.ax.tick_params(labelsize='small')
+    cax4 = fig.add_axes([0.02 + 4 * 0.196, 0.05, 0.18, 0.01])
+    cb4 = fig.colorbar(i4, cax=cax4, orientation='horizontal')
+    cb4.ax.tick_params(labelsize='small')
+
+    for axx in ax.ravel():
+        axx.set_axis_off()
+
+    ax[0, 0].set_title('1H T1', fontsize='small')
+    ax[0, 1].set_title('23Na conventional', fontsize='small')
+    ax[0, 2].set_title('23Na AGR wo decay', fontsize='small')
+    ax[0, 3].set_title('23Na AGR w decay', fontsize='small')
+    ax[0, 4].set_title('est. decay r', fontsize='small')
+
+    fig.tight_layout(pad=1.8)
+    fig.show()
+
+    q.append(agr_w_decay[bbox])
 #------------------------------------------------------------------------------
-# flip all images to LPS
 
-t1 = np.flip(t1, (0, 1))
-conv = np.flip(conv, (0, 1))
-agr_wo_decay = np.flip(agr_wo_decay, (0, 1))
-agr_w_decay = np.flip(agr_w_decay, (0, 1))
-est_ratio = np.flip(est_ratio, (0, 1))
-aparc = np.flip(aparc, (0, 1))
+ratio_df = dfs['cortical GM'] / dfs['WM']
 
-#------------------------------------------------------------------------------
-# flip define ROIs and quantify
+plt.style.use('default')
 
-agr_wo_means = OrderedDict()
-agr_w_means = OrderedDict()
-conv_means = OrderedDict()
+fig2, ax2 = plt.subplots(1, 3, figsize=(9, 3))
+dfs['cortical GM'].plot(ax=ax2[0], rot=0, kind='bar', legend=False)
+dfs['WM'].plot(ax=ax2[1], rot=0, kind='bar', legend=False)
+ratio_df.plot(ax=ax2[2], rot=0, kind='bar').legend(loc='lower right')
 
-roi_inds = OrderedDict()
-cortical_gm_mask = (aparc >= 1000).astype(np.uint8)
-wm_mask = (aparc == 2).astype(np.uint8) + (aparc == 41).astype(np.uint8)
-# WM next to GM
-wm_gm_mask = binary_dilation(cortical_gm_mask, iterations=2) * wm_mask
-central_wm_mask = binary_erosion(wm_mask, iterations=5)
-ventricle_mask = (aparc == 4).astype(np.uint8) + (aparc == 43).astype(np.uint8)
+ax2[0].set_title('GM', fontsize='medium')
+ax2[1].set_title('WM', fontsize='medium')
+ax2[2].set_title('GM/WM ratio', fontsize='medium')
 
-roi_inds['cortical GM'] = np.where(cortical_gm_mask)
-roi_inds['WM'] = np.where(wm_mask)
-roi_inds['WM next to GM'] = np.where(wm_gm_mask)
-roi_inds['central WM'] = np.where(central_wm_mask)
-roi_inds['ventricles'] = np.where(ventricle_mask)
+ax2[0].set_ylabel('TSC (arb. units)', fontsize='medium')
+ax2[1].set_ylabel('TSC (arb. units)', fontsize='medium')
 
-for roi, inds in roi_inds.items():
-    agr_wo_means[roi] = agr_wo_decay[inds].mean()
-    agr_w_means[roi] = agr_w_decay[inds].mean()
-    conv_means[roi] = conv[inds].mean()
+ax2[0].set_ylim(0, 0.95)
+ax2[1].set_ylim(0, 0.95)
 
-df = pd.DataFrame({
-    'conventional': pd.Series(conv_means),
-    'AGR wo decay model': pd.Series(agr_wo_means),
-    'AGR w decay model': pd.Series(agr_w_means)
-})
+for axx in ax2.ravel():
+    axx.set_xlabel('subject')
+    axx.grid(ls=':')
 
-df.loc['cortical GM / WM ratio'] = df.loc['cortical GM'] / df.loc['WM']
-df.loc['cortical GM / WM next to GM ratio'] = df.loc['cortical GM'] / df.loc[
-    'WM next to GM']
-df.loc['cortical GM / central WM ratio'] = df.loc['cortical GM'] / df.loc[
-    'central WM']
-df.loc[
-    'cortical GM / ventricles'] = df.loc['cortical GM'] / df.loc['ventricles']
-df.loc['WM / ventricles'] = df.loc['WM'] / df.loc['ventricles']
-
-print(df)
-
-fig, ax = plt.subplots(figsize=(8, 4))
-df.plot(kind='barh', ax=ax)
-ax.grid(ls=':')
-fig.tight_layout()
-fig.show()
-
-#------------------------------------------------------------------------------
-
-import pymirc.viewer as pv
-
-bbox = find_objects(t1 > 0.1 * np.percentile(t1, 99.9))[0]
-
-ims = [
-    dict(cmap='Greys_r', vmin=0, vmax=np.percentile(t1, 99.99))
-] + 3 * [dict(cmap='Greys_r', vmin=0, vmax=np.percentile(agr_w_decay, 99.9))
-         ] + [dict(cmap='Greys_r', vmin=0, vmax=1)]
-
-vi = pv.ThreeAxisViewer([
-    t1[bbox], conv[bbox], agr_wo_decay[bbox], agr_w_decay[bbox],
-    est_ratio[bbox]
-],
-                        imshow_kwargs=ims)
+fig2.tight_layout()
+fig2.show()
